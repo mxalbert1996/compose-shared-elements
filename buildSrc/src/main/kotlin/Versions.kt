f3b9c035955acd4ea693681d34c2b751f37bb5d0
Albert Chang <mxalbert1996@gmail.com>
2021-03-25 12:02:27 +0900

    Upgrade to Kotlin 1.4.31 and Compose 1.0.0-beta03

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,7 +1,7 @@
 object Versions {
 
     object Project {
-        const val Name = "0.1.0-beta01"
+        const val Name = "0.1.0-beta03"
         const val Code = 1
     }
 
@@ -12,14 +12,14 @@ object Versions {
     }
 
     const val BuildTools = "30.0.3"
-    const val AGP = "7.0.0-alpha08"
-    const val Kotlin = "1.4.30"
-    const val Compose = "1.0.0-beta01"
+    const val AGP = "7.0.0-alpha11"
+    const val Kotlin = "1.4.31"
+    const val Compose = "1.0.0-beta03"
 
     object AndroidX {
-        const val Core = "1.5.0-beta02"
-        const val AppCompat = "1.3.0-beta01"
-        const val Activity = "1.3.0-alpha03"
+        const val Core = "1.5.0-rc01"
+        const val AppCompat = "1.3.0-rc01"
+        const val Activity = "1.3.0-alpha05"
     }
 
     const val JUnit = "4.13"
